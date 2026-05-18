c357f28345673c12c454e675ecc4cdddd1fa3d41
Leko <leko@leko.moe>
2026-05-18 19:38:18 +0800

    feat: add Android ARM implementation package

---
+++ packages/cloudflared_tunnel_android_arm/lib/cloudflared_tunnel_android_arm.dart
@@ -0,0 +1,4 @@
+/// Android ARM implementation package for cloudflared_tunnel_full.
+///
+/// The native Android plugin is registered through the Android embedding.
+library cloudflared_tunnel_android_arm;
