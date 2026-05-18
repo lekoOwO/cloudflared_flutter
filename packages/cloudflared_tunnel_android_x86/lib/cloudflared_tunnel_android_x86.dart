a5304af25ed7e89d16295c96cc6fe3c41f181e32
Leko <leko@leko.moe>
2026-05-18 19:38:44 +0800

    feat: add Android x86 sidecar package

---
+++ packages/cloudflared_tunnel_android_x86/lib/cloudflared_tunnel_android_x86.dart
@@ -0,0 +1,5 @@
+/// Android x86 sidecar package for cloudflared_tunnel_full.
+///
+/// This package contributes x86 and x86_64 JNI libraries to Android builds.
+/// It intentionally exposes no runtime Dart API.
+library cloudflared_tunnel_android_x86;
